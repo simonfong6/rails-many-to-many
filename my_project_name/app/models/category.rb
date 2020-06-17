b0a7aad3fa74c058c0b48dacd7d928f21413ac74
Simon <simonfong6@gmail.com>
2020-06-16 22:25:01 -0700

    added things

--- my_project_name/app/models/category.rb
+++ my_project_name/app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  belongs_to :articles
+  has_and_belongs_to_many :articles
 end
--- my_project_name/db/schema.rb
+++ my_project_name/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_050843) do
+ActiveRecord::Schema.define(version: 2020_06_17_051534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,19 @@ ActiveRecord::Schema.define(version: 2020_06_17_050843) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.bigint "article_id"
+    t.bigint "category_id"
+    t.index ["article_id"], name: "index_articles_categories_on_article_id"
+    t.index ["category_id"], name: "index_articles_categories_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
